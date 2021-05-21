eb11144cfd47b62b0bffe3c8f999ed79eab859ce
malgascio <malgascio@hotmail.com>
2021-05-21 18:04:15 +0200

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -12,6 +13,7 @@ export class CartComponent implements OnInit {
 
   constructor(
     private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
